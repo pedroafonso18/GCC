93e103abd0bf6e387bfc4641ad045b6190b9a713
pedroafonso18 <pedroafonsoprogramador@gmail.com>
2025-04-10 19:35:41 -0300

    feat: Added flags and other bit-mapping

--- src/cpu.c
+++ src/cpu.c
@@ -4,7 +4,37 @@ uint16_t get_bc(CPU* cpu) {
     return(cpu->b << 8) | cpu->c;
 }
 
+uint16_t get_af(CPU* cpu) {
+    return(cpu->a << 8) | cpu->flag;
+}
+
+uint16_t get_de(CPU* cpu) {
+    return(cpu->d << 8) | cpu->e;
+}
+
 void set_bc(CPU* cpu, uint16_t value) {
     cpu->b = (value >> 8) & 0xFF;
     cpu->c = value & 0xFF;
+}
+
+void set_af(CPU* cpu, uint16_t value) {
+    cpu->a = value >> 8;
+    cpu->flag = value & 0xF0;
+}
+
+void set_de(CPU* cpu, uint16_t value) {
+    cpu->d = (value >> 8) & 0xFF;
+    cpu->e = value & 0xFF;
+}
+
+void set_flag(CPU* cpu, uint8_t flag) {
+    cpu->flag |= flag;
+}
+
+void clear_flag(CPU* cpu, uint8_t flag) {
+    cpu->flag &= ~flag;
+}
+
+bool is_flag_set(CPU* cpu, uint8_t flag) {
+    return cpu->flag & flag;
 }
--- src/cpu.h
+++ src/cpu.h
@@ -1,16 +1,38 @@
 #ifndef CPU_H
 #define CPU_H
 
-#include <stdint.h>
+#define FLAG_Z (1 << 7)
+#define FLAG_N (1 << 6)
+#define FLAG_H (1 << 5)
+#define FLAG_C (1 << 4)
 
-typedef struct {
-    uint8_t a, b, c, d, e, h, l;
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct
+{
+    uint8_t a, b, c, d, e, h, l, flag;
     uint16_t sp;
     uint16_t pc;
 } CPU;
 
-uint16_t get_bc(CPU* cpu);
+uint16_t get_bc(CPU *cpu);
 
-void set_bc(CPU* cpu, uint16_t value);
+uint16_t get_af(CPU *cpu);
+
+uint16_t get_de(CPU *cpu);
+
+
+void set_af(CPU *cpu, uint16_t value);
+
+void set_de(CPU *cpu, uint16_t value);
+
+void set_bc(CPU *cpu, uint16_t value);
+
+void set_flag(CPU *cpu, uint8_t flag);
+
+void clear_flag(CPU *cpu, uint8_t flag);
+
+bool is_flag_set(CPU *cpu, uint8_t flag);
 
 #endif
